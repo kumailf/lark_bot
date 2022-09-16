31c0f33a672d3d6049f4fe44ec2ff4d6d659b109
kumailf <kumailf77@gmail.com>
2022-09-16 11:30:51 +0800

    update appid and secret

--- conf/config.go
+++ conf/config.go
@@ -10,8 +10,8 @@ var Conf *Config
 
 // replace conf when in use
 const (
-	appID      = "cli_a3aa9f78617c100c"
-	secret     = "B72Y4SPSWf2GCoxH0hz7s2hYM5duQ7vJ"
+	appID      = "cli_a234d72466f9d00b"
+	secret     = "LFZzsDBKtVeKQibIzQ4eviR2SkhWd4e8"
 	encryptKey = "bebKa2lcEIrcQpCBhEaOyezSwuolyaDf"
 )
 
