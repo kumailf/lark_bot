c7ada7eebedfaac48429a12bb223e9e221a6abc0
kumailf <kumailf77@gmail.com>
2022-11-07 16:26:00 +0800

    fix

--- biz/github_raw_event.go
+++ biz/github_raw_event.go
@@ -73,8 +73,8 @@ func HandleReceiveGithubIssueEvent(ctx context.Context, event *ReceiveGithubIssu
 
 func HandleReceiveGithubPREvent(ctx context.Context, event *ReceiveGithubPREvent) error {
 	logrus.Info("receive github raw event")
-	repo_fullname := event.Event.Repo.GetFullName()
-
+	pr := event.Event
+	repo_fullname := pr.Repo.GetFullName()
 	// Set Project
 	go func() {
 		if repo_fullname == "kumailf/MyPic" || repo_fullname == "open-mmlab/mmediting" {
@@ -93,5 +93,51 @@ func HandleReceiveGithubPREvent(ctx context.Context, event *ReceiveGithubPREvent
 		}
 	}()
 
+	go func() {
+		eventType := pr.GetAction()
+		var content string
+		switch eventType {
+		case "opened":
+			prTitle := pr.PullRequest.GetTitle()
+			openBy := pr.PullRequest.User.GetLogin()
+			prUrl := pr.PullRequest.GetHTMLURL()
+			content = fmt.Sprintf("{\"config\":{\"wide_screen_mode\":true},\"elements\":[{\"tag\":\"div\",\"text\":{\"content\":\"** PR Title: **%v\\n** Opened By: **%v\\n** Link: **%v\",\"tag\":\"lark_md\"}}],\"header\":{\"template\":\"green\",\"title\":{\"content\":\"New PullRequest\",\"tag\":\"plain_text\"}}}", prTitle, openBy, prUrl)
+		case "closed":
+			merged := pr.PullRequest.GetMerged()
+			if merged {
+				prTitle := pr.PullRequest.GetTitle()
+				openBy := pr.PullRequest.User.GetLogin()
+				mergedBy := pr.PullRequest.MergedBy.GetLogin()
+				prUrl := pr.PullRequest.GetHTMLURL()
+				content = fmt.Sprintf("{\"config\":{\"wide_screen_mode\":true},\"elements\":[{\"tag\":\"div\",\"text\":{\"content\":\"** PR Title: **%v\\n** Opened By: **%v\\n** Merged By: **%v\\n** Link: **%v\",\"tag\":\"lark_md\"}}],\"header\":{\"template\":\"green\",\"title\":{\"content\":\"PullRequest Merged\",\"tag\":\"plain_text\"}}}", prTitle, openBy, mergedBy, prUrl)
+			} else {
+				return
+			}
+		default:
+			return
+		}
+		groupName, ok := conf.GroupMap[repo_fullname]
+		if !ok {
+			groupName = "机器人调试"
+		}
+		receiveID, err := GetGroupID(groupName)
+		if err != nil {
+			logrus.WithError(err).Errorf("failed to get group id")
+		}
+		token, err := GetTenantAccessToken(ctx)
+		if err != nil {
+			logrus.WithError(err).Errorf("failed to get tenant access token")
+		}
+		createMsgRequest := &CreateMessageRequest{
+			ReceiveID: receiveID,
+			Content:   content,
+			MsgType:   "interactive",
+		}
+		resp, err := SendMessage(ctx, token, createMsgRequest)
+		if err != nil {
+			logrus.WithError(err).Errorf("failed to send msg")
+		}
+		logrus.Infof("succeed send msg, msg_id: %v", resp.MessageID)
+	}()
 	return nil
 }
