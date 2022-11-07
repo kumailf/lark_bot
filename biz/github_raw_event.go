2defc6f13c587df8c50a388871fef8e9ce690168
kumailf <kumailf77@gmail.com>
2022-11-07 16:08:44 +0800

    fix

--- biz/github_raw_event.go
+++ biz/github_raw_event.go
@@ -15,58 +15,53 @@ func HandleReceiveGithubIssueEvent(ctx context.Context, event *ReceiveGithubIssu
 	ie := event.Event
 	repo_fullname := ie.Repo.FullName
 	eventType := ie.Action
-	// Set Project
-	go func() {
-		if repo_fullname == "kumailf/MyPic" || repo_fullname == "open-mmlab/mmediting" {
-			token := ""
-			project_id := ""
-			node_id := ie.Issue.NodeID
-			if repo_fullname == "kumailf/MyPic" {
-				token = os.Getenv("token_MyPic")
-				project_id = os.Getenv("projectId_MyPic")
-			}
-			if repo_fullname == "open-mmlab/mmediting" {
-				token = os.Getenv("token_mmediting")
-				project_id = os.Getenv("projectId_mmediting")
-			}
-			_ = SetProject(token, project_id, node_id)
-		}
-	}()
 
-	// Send Message To Lark Group
-	go func() {
-		switch eventType {
-		case "opened":
-			issueTitle := ie.Issue.Title
-			createBy := ie.Issue.User.Login
-			issueUrl := ie.Issue.HTMLURL
-			groupName, ok := conf.GroupMap[repo_fullname]
-			if !ok {
-				groupName = "机器人调试"
-			}
-			receiveID, err := GetGroupID(groupName)
-			if err != nil {
-				logrus.WithError(err).Errorf("failed to get group id")
-			}
-			token, err := GetTenantAccessToken(ctx)
-			if err != nil {
-				logrus.WithError(err).Errorf("failed to get tenant access token")
-			}
-			content := fmt.Sprintf("{\"config\":{\"wide_screen_mode\":true},\"elements\":[{\"tag\":\"div\",\"text\":{\"content\":\"** Issue Title: **%v\\n** Created By: **%v\\n** Link: **%v\",\"tag\":\"lark_md\"}}],\"header\":{\"template\":\"green\",\"title\":{\"content\":\"New Issue\",\"tag\":\"plain_text\"}}}", issueTitle, createBy, issueUrl)
-			createMsgRequest := &CreateMessageRequest{
-				ReceiveID: receiveID,
-				Content:   content,
-				MsgType:   "interactive",
-			}
-			resp, err := SendMessage(ctx, token, createMsgRequest)
-			if err != nil {
-				logrus.WithError(err).Errorf("failed to send msg")
-			}
-			logrus.Infof("succeed send msg, msg_id: %v", resp.MessageID)
-		default:
-			return
+	// Set Project
+	if repo_fullname == "kumailf/MyPic" || repo_fullname == "open-mmlab/mmediting" {
+		token := ""
+		project_id := ""
+		node_id := ie.Issue.NodeID
+		if repo_fullname == "kumailf/MyPic" {
+			token = os.Getenv("token_MyPic")
+			project_id = os.Getenv("projectId_MyPic")
 		}
-	}()
+		if repo_fullname == "open-mmlab/mmediting" {
+			token = os.Getenv("token_mmediting")
+			project_id = os.Getenv("projectId_mmediting")
+		}
+		_ = SetProject(token, project_id, node_id)
+	}
+	// Send Message To Lark Group
+
+	switch eventType {
+	case "opened":
+		issueTitle := ie.Issue.Title
+		createBy := ie.Issue.User.Login
+		issueUrl := ie.Issue.HTMLURL
+		groupName, ok := conf.GroupMap[repo_fullname]
+		if !ok {
+			groupName = "机器人调试"
+		}
+		receiveID, err := GetGroupID(groupName)
+		if err != nil {
+			logrus.WithError(err).Errorf("failed to get group id")
+		}
+		token, err := GetTenantAccessToken(ctx)
+		if err != nil {
+			logrus.WithError(err).Errorf("failed to get tenant access token")
+		}
+		content := fmt.Sprintf("{\"config\":{\"wide_screen_mode\":true},\"elements\":[{\"tag\":\"div\",\"text\":{\"content\":\"** Issue Title: **%v\\n** Created By: **%v\\n** Link: **%v\",\"tag\":\"lark_md\"}}],\"header\":{\"template\":\"green\",\"title\":{\"content\":\"New Issue\",\"tag\":\"plain_text\"}}}", issueTitle, createBy, issueUrl)
+		createMsgRequest := &CreateMessageRequest{
+			ReceiveID: receiveID,
+			Content:   content,
+			MsgType:   "interactive",
+		}
+		resp, err := SendMessage(ctx, token, createMsgRequest)
+		if err != nil {
+			logrus.WithError(err).Errorf("failed to send msg")
+		}
+		logrus.Infof("succeed send msg, msg_id: %v", resp.MessageID)
+	}
 
 	return nil
 }
