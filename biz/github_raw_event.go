fd7ae3d31dd7b2deab213f03cea2589c0b52d029
kumailf <kumailf77@gmail.com>
2022-11-09 16:01:41 +0800

    add log info

--- biz/github_raw_event.go
+++ biz/github_raw_event.go
@@ -165,6 +165,8 @@ func HandleReceiveGithubPREvent(ctx context.Context, event *ReceiveGithubPREvent
 			} else {
 				prReviewer = fmt.Sprintf(`<at user_id=\\\"%v\\\">%v</at>`, user_id, login)
 			}
+			logrus.Infof("prReviewer == ")
+			logrus.Infof(prReviewer)
 			content = fmt.Sprintf("{\"config\":{\"wide_screen_mode\":true},\"elements\":[{\"tag\":\"div\",\"text\":{\"content\":\"** PR Title: **%v\\n** Reviewer: **%v\\n** Link: **%v\",\"tag\":\"lark_md\"}}],\"header\":{\"template\":\"green\",\"title\":{\"content\":\"PullRequest Requeste Reviewer\",\"tag\":\"plain_text\"}}}", prTitle, prReviewer, prUrl)
 		default:
 			return
