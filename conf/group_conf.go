70c04f3e84a8310cb0e7b3ef0557184a435a1ec2
Jaylin Lee <apx103@foxmail.com>
2022-11-10 10:57:08 +0800

    [update config] Add new group map

--- conf/group_conf.go
+++ conf/group_conf.go
@@ -4,12 +4,13 @@ var GroupMap map[string]string
 
 func init() {
 	GroupMap = map[string]string{
-		"kumailf/MyPic":          "机器人调试",
-		"del-zhenwu/easy_http":   "开源测试",
-		"open-mmlab/mmediting":   "Low-level Vision",
-		"open-mmlab/mmocr":       "MMOCR-OpenMMLab分部",
-		"open-mmlab/mmdetection": "2.0 适配-Det",
+		"kumailf/MyPic":             "机器人调试",
+		"del-zhenwu/easy_http":      "开源测试",
+		"open-mmlab/mmediting":      "Low-level Vision",
+		"open-mmlab/mmocr":          "MMOCR-OpenMMLab分部",
+		"open-mmlab/mmdetection":    "2.0 适配-Det",
+		"open-mmlab/mmflow":         "接收 mmflow/mmseg Github 消息",
+		"open-mmlab/mmsegmentation": "接收 mmflow/mmseg Github 消息",
 		// "open-mmlab/mmdetection3d": "",
 	}
 }
-
