a3a7b2796c5a4fad19efbe277903ccf43483809b
ZhengShaoFeng <49893630+kumailf@users.noreply.github.com>
2024-09-04 16:09:06 +0800

    remove config

--- conf/config.go
+++ conf/config.go
@@ -10,9 +10,9 @@ var Conf *Config
 
 // replace conf when in use
 const (
-	appID      = "cli_a234d72466f9d00b"
-	secret     = "LFZzsDBKtVeKQibIzQ4eviR2SkhWd4e8"
-	encryptKey = "bebKa2lcEIrcQpCBhEaOyezSwuolyaDf"
+	appID      = "**"
+	secret     = "**"
+	encryptKey = "**"
 )
 
 func init() {
